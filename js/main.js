6e39c20deecfd27327b288ddbc9179bc93d0cc58
Anna Suiter <acsuiter@gmail.com>
2019-04-13 16:04:25 -0500

    toggle stuff
    will this ever be fixed

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 $('#toggle').click(function() {
-	$('#rules').toggle();
+	console.log($('.rules').css('display'));
+	console.log('click');
+	$('.rules').toggle();
 });
 
 var waterPercent = 0;
@@ -7,6 +9,24 @@ var waterPercent = 0;
 $('.5percent').click(function() {
     $('.inner-glass').height(function(){
     	waterPercent += 5;
+    	console.log(waterPercent);
+    	rightPercent = waterPercent + '%';
+      	return rightPercent;
+    });
+});
+
+$('.10percent').click(function() {
+    $('.inner-glass').height(function(){
+    	waterPercent += 10;
+    	console.log(waterPercent);
+    	rightPercent = waterPercent + '%';
+      	return rightPercent;
+    });
+});
+
+$('.15percent').click(function() {
+    $('.inner-glass').height(function(){
+    	waterPercent += 15;
     	console.log(waterPercent);
     	rightPercent = waterPercent + '%';
       	return rightPercent;
