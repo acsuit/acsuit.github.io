728f366e83b5fc181245dc9283cbd0720c1b2ff4
Anna Suiter <acsuiter@gmail.com>
2019-04-12 19:10:01 -0500

    js cleanup and some name changes

--- js/main.js
+++ js/main.js
@@ -1,16 +1,5 @@
-var coll = document.getElementsByClassName("collapse");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-} 
+$('#toggle').click(function() {
+	$('#rules').toggle();
+});
 
 var waterPercent = 0;
