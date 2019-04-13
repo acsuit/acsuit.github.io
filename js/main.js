d281d7206b4ab27f277e1d12b0bc4eaeb12749a3
Anna Suiter <acsuiter@gmail.com>
2019-04-13 11:01:54 -0500

    glass
    it fills! kind of

--- js/main.js
+++ js/main.js
@@ -3,3 +3,9 @@ $('#toggle').click(function() {
 });
 
 var waterPercent = 0;
+
+$('.5percent').click(function() {
+    $('.inner-glass').height(function(n,waterPercent){
+      return waterPercent+5 + '%';
+    });
+});
