78bda5664e2ca813138cbdf3d1a3bcfcf1393ceb
Anna Suiter <acsuiter@gmail.com>
2019-04-14 19:11:50 -0500

    fonts and some colors

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 $('#toggle').click(function() {
-	console.log($('.rules').css('display'));
-	console.log('click');
 	$('.container').toggle();
 });
 
@@ -39,18 +37,14 @@ $('.15percent').click(function() {
 });
 
 $('.tasks button').click(function(){ 
-	console.log('work bitch');
 	if (waterPercent < 100) {
-		console.log('hi');
 		$('.score').html('<p>KEEP GOING! '  + waterPercent + '</p>');
 	} else if (waterPercent > 100) {
-		console.log('lose');
 		$('.tasks button').attr('disabled', 'disabled');
 		$('.score').html('<p>YOU LOSE! '  + waterPercent + '</p>');
 	} else {
 		$('.score').html('<p>YOU WIN! '  + waterPercent + '</p>');
 		$('.tasks button').attr('disabled', 'disabled');
-		console.log('win');
 	};
 });
 
@@ -62,8 +56,5 @@ $('#reset-button').click(function() {
       	return waterLevel;
     });
     $('.score').html('<p>RESET!' + waterPercent + '</p>');
-    $('.tasks button').attr('disabled', falses);
-    console.log('inner ' + waterPercent);
+    $('.tasks button').attr('disabled', false);
 });
-
-console.log(waterPercent);
